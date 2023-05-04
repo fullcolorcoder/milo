bc4534155346274bc68ffa5ab927ee2ae784eab7
Axel Cureno Basurto <axel@adobe.com>
2023-05-04 13:54:45 -0700

    wip

--- libs/blocks/commerce/commerce.js
+++ libs/blocks/commerce/commerce.js
@@ -4,17 +4,7 @@ import { loadTacocat } from '../merch/merch.js';
 const decorateOfferDetails = (el, offer) => {
   const offerDetails = document.createElement('ul');
   offerDetails.className = 'offer-details';
-  // const offer = {
-  //   'Type': offer.type,
-  //   'Offer ID': offer.offerID,
-  //   'Product Arrangement': offer.productArrangement,
-  //   'Price Point': offer.pricePoint,
-  //   'Customer Segment': offer.customerSegment,
-  //   'Commitment': offer.commitment,
-  //   'Term': offer.term,
-  //   'Offer Type': offer.offerType,
-  //   'OSI': offer.OSI
-  // };
+
   if (offer !== null && typeof offer === 'object') {
     Object.entries(offer).forEach(([key, value]) => {
       const li = document.createElement('li');
@@ -26,45 +16,45 @@ const decorateOfferDetails = (el, offer) => {
 };
 
 const handleSearch = (e, els) => {
+  // const offer = {
+  //   type: 'Type 1',
+  //   offerID: '123456',
+  //   productArrangement: 'Product 1',
+  //   pricePoint: 'Price 1',
+  //   customerSegment: 'Segment 1',
+  //   commitment: 'Commitment 1',
+  //   term: 'Term 1',
+  //   offerType: 'Type 2',
+  //   OSI: 'OSI 1',
+  //   price: '$49.99',
+  //   checkoutURL: 'https://www.google.com',
+  // };
   const search = e.target.value;
-  loadTacocat();
-  const { searchParams } = new URL(e.href);
+  try {
+    loadTacocat();
+  } catch (error) {
+    console.error('Tacocat not loaded', error);
+    return undefined;
+  }
+  const { searchParams } = new URL(search);
   const osi = searchParams.get('osi');
   const type = searchParams.get('type');
   if (!(osi && type)) {
     e.textContent = '';
     e.setAttribute('aria-details', 'Invalid URL');
+    console.log('Invalid URL');
     return undefined;
   }
-
-  els.forEach((subject) => {
-    if (subject.textContent.includes(search)) {
-      subject.style.display = 'flex';
-    } else {
-      subject.style.display = 'none';
-    }
-  });
+  console.log('osi', osi);
+  console.log('osi', type);
 };
 
 const decorateSearch = (el) => {
-  const search = createTag('input', { class: 'offer-search', placeholder: 'Search offer' });
+  const search = createTag('input', { class: 'offer-search', placeholder: 'Search offer', value: 'https://ost--milo--adobecom.hlx.page/tools/ost?osi=Mutn1LYoGojkrcMdCLO7LQlx1FyTHw27ETsfLv0h8DQ&offerId=01A09572A72A7D7F848721DE4D3C73FA&type=price&term=true&seat=true&tax=false' });
   const icon = createTag('div', { class: 'offer-search-icon' });
   const wrapper = createTag('div', { class: 'offer-search-wrapper' }, [search, icon]);
-  const offer = {
-    type: 'Type 1',
-    offerID: '123456',
-    productArrangement: 'Product 1',
-    pricePoint: 'Price 1',
-    customerSegment: 'Segment 1',
-    commitment: 'Commitment 1',
-    term: 'Term 1',
-    offerType: 'Type 2',
-    OSI: 'OSI 1',
-    price: '$49.99',
-    checkoutURL: 'https://www.google.com',
-  };
-  const offerDetails = decorateOfferDetails(el, offer);
-  search.addEventListener('keyup', (e) => { handleSearch(e, offerDetails); });
+
+  search.addEventListener('keyup', (e) => { handleSearch(e); });
   el.append(wrapper, offerDetails);
 };
 
