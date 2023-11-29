922f4b6e7f24d590893c804711e4ca234b282281
Cody Lloyd <colloyd@adobe.com>
2023-11-29 11:51:33 -0700

    original everything

--- libs/blocks/quiz/quizoption.js
+++ libs/blocks/quiz/quizoption.js
@@ -1,62 +1,40 @@
 import { html } from '../../deps/htm-preact.js';
 
 export const OptionCard = ({
-  text, title, image, icon, iconTablet, iconDesktop, options, disabled, selected, background,
+  text, title, coverImage, cardIcon, options, disabled, selected,
 }) => {
-  const getOptionClass = () => {
-    let className = '';
-    if (icon || iconTablet || iconDesktop) className += 'has-icon ';
-    if (image) className += 'has-image ';
-    if (background) className += 'has-background ';
-    if (disabled) className += disabled;
-    if (selected) className += selected;
-    return className;
-  };
-
-  const getIconHtml = (iconUrl, iconClass) => html`<div class="quiz-option-icon ${iconClass}">
-    <img src="${iconUrl}" alt="Quiz Option Icon" />
+  const cardIconHtml = html`<div class="quiz-option-icon">
+    <img src="${cardIcon}" alt="Quiz Option Icon" />
   </div>`;
 
-  const imageHtml = html`
+  const coverImageHtml = html`
     <div class="quiz-option-image" 
-      style="background-image: url('${image}')">
+      style="background-image: url('${coverImage}')">
     </div>`;
 
-  const titleHtml = html`
-    <h3 class="quiz-option-title">${title}</h3>
-  `;
-
-  const textHtml = html`
-    <p class="quiz-option-text">${text}</p>
-  `;
-
-  return html`<button class="quiz-option ${getOptionClass()}" data-option-name="${options}" 
+  return html`<button class="quiz-option ${disabled}${selected}" data-option-name="${options}" 
+        data-option-type="${cardIcon ? 'icon' : ''}${coverImage ? 'cover-image' : ''}"
         aria-pressed="${!!selected}" tabindex="${disabled ? '-1' : '0'}">
-        ${iconDesktop && getIconHtml(iconDesktop, 'icon-desktop')}
-        ${iconTablet && getIconHtml(iconTablet, 'icon-tablet')}
-        ${icon && getIconHtml(icon, 'icon-default')}
-        ${image && imageHtml}
+        ${cardIcon && cardIconHtml}
+        ${coverImage && coverImageHtml}
         <div class="quiz-option-text-container">  
-          ${title && titleHtml}
-          ${text && textHtml}
+          <h3 class="quiz-option-title">${title}</h3>
+          <p class="quiz-option-text">${text}</p>
         </div>
     </button>`;
 };
 
 export const CreateOptions = ({
   options, handleCardSelection, selectedCards, countSelectedCards = 0, maxSelections,
-  getOptionsIcons, background,
+  getOptionsIcons,
 }) => html`
       ${options?.data.map((option, index) => (
     html`<div key=${index} onClick=${handleCardSelection(option)}>
           <${OptionCard} 
             text=${option.text}
             title=${option.title} 
-            icon=${getOptionsIcons(option.options, 'icon')}
-            iconTablet=${getOptionsIcons(option.options, 'icon-tablet')}
-            iconDesktop=${getOptionsIcons(option.options, 'icon-desktop')}
-            image=${getOptionsIcons(option.options, 'image')}
-            background=${background}
+            cardIcon=${getOptionsIcons(option.options, 'image')}
+            coverImage=${getOptionsIcons(option.options, 'cover')}
             options=${option.options}
             selected=${selectedCards[option.options] ? 'selected' : ''}
             disabled=${(countSelectedCards > 0 && !selectedCards[option.options] && countSelectedCards >= maxSelections) ? 'disabled' : ''}/>
@@ -66,7 +44,7 @@ export const CreateOptions = ({
 export const GetQuizOption = ({
   btnText, options, minSelections, maxSelections, selectedCards,
   handleOnNextClick, onOptionClick, countSelectedCards, getOptionsIcons,
-  btnAnalyticsData, background,
+  btnAnalyticsData,
 }) => html`
   <div class="quiz-question">
       <div class="quiz-options-container">
@@ -76,8 +54,7 @@ export const GetQuizOption = ({
           countSelectedCards=${countSelectedCards} 
           maxSelections=${maxSelections}
           getOptionsIcons=${getOptionsIcons}
-          handleCardSelection=${onOptionClick}
-          background=${background} />
+          handleCardSelection=${onOptionClick} />
       </div>
       <div class="quiz-button-container">
         <button 
@@ -90,3 +67,4 @@ export const GetQuizOption = ({
         </button>
       </div>
   </div>`;
+  
